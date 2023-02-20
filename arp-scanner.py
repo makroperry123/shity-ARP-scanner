b2e026952f754636766ce9d0f56fc605014b371d
alpar <alparcagan2000@gmail.com>
2023-02-20 13:15:20 +0300

    python file pushed

---
+++ arp-scanner.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 19 22:40:22 2023
+
+@author: alpar
+"""
+
+from scapy.all import Ether,ARP,conf,srp
+import sys
+from datetime import datetime
+from win_nic import NetworkAdapters
+
+
+this_pc_nics = NetworkAdapters()
+ethernet_nic = this_pc_nics.get_nic(index = 10)
+
+
+INTERFACE =ethernet_nic.description
+
+def ARP_Scan(ips):
+    
+
+	print("[*] Scanning...") 
+	start_time = datetime.now()
+
+	conf.verb = 0 
+	ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), 
+		     timeout = 2, 
+		     iface = INTERFACE,
+		     inter = 0.1)
+
+	print ("\n[*] IP - MAC") 
+	for snd,rcv in ans: 
+		print(rcv.sprintf(r"%ARP.psrc% - %Ether.src%"))
+	stop_time = datetime.now()
+	total_time = stop_time - start_time 
+	print("\n[*] Scan Complete. Duration:", total_time)
+
+if __name__ == "__main__":
+    ARP_Scan(sys.argv[1])   
+
+#%%
+from win_nic import NetworkAdapters
+
+
+this_pc_nics = NetworkAdapters()
+ethernet_nic = this_pc_nics.get_nic(index = 10)
+
+print(ethernet_nic)
+
+
+
